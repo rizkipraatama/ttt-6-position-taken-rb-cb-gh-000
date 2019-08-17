0c3b056f9012d19c655cf3e6380caf46ef439533
Rizki Pratama <rizkipratama0602@gmail.com>
2019-08-17 19:51:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,9 @@
 # code your #position_taken? method here!
-def changetoindex(input)
-  newint = input.to_i
-  return newint - 1
-end
 
-def position_taken(array, index)
-  if array[index] == " "
-    return true
-  else
+def position_taken(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
     return false
   end
 end
