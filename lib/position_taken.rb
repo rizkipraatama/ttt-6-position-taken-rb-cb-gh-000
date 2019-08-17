be59877e2973c1386112ef49c536c20180145483
Rizki Pratama <rizkipratama0602@gmail.com>
2019-08-17 19:53:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,5 +5,9 @@ def position_taken(board, index)
     return false
   elsif board[index] == ""
     return false
+  elsif board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return trus
   end
 end
